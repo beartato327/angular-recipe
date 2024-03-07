adcd272befd1bf9dac651e0b94046b76a3a858b8
beartato327 <recapurro@outlook.com>
2024-03-07 16:07:59 -0600

    add recipes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-
+import { RecipesComponent } from './recipes/recipes.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, RecipesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
