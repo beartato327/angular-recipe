da079dd892d05a09344ebd4ab8bb57e31a1323d4
Roberto Capurro <recapurro327@gmail.com>
2024-03-08 08:43:17 -0600

    add card and started design

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css'
 })
