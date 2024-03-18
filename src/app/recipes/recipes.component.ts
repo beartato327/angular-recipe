86dec6c32865355fc247b041aa48bc0c0ef2b42c
Roberto Capurro <recapurro327@gmail.com>
2024-03-18 12:52:06 -0500

    update to recipe

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Recipe } from '../models/recipe.model';
+import { MatChipsModule } from '@angular/material/chips';
+import { CommonModule } from '@angular/common';
+import { RecipeService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule],
+  imports: [MatButtonModule, MatCardModule, MatChipsModule, CommonModule],
+  providers: [RecipeService],
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.css'
+  styleUrl: './recipes.component.css',
 })
-export class RecipesComponent {
+export class RecipesComponent implements OnInit {
+  recipes: Recipe[] = [];
 
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit() {
+    return (this.recipes = this.recipeService.getRecipes());
+  }
 }
