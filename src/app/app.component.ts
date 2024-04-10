65e0b6e436d36af1867a182ff745a8ddbe74c4db
Roberto Capurro <recapurro327@gmail.com>
2024-04-09 22:41:58 -0500

    Added routing, auth and 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { AuthComponent } from './auth/auth.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RecipesComponent],
+  imports: [RouterOutlet, HeaderComponent, RecipesComponent, AuthComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: '/auth', pathMatch: 'full'},
+    {path: 'auth', component: AuthComponent},
+    {path: 'recipes', component: RecipesComponent},
+    {path: '**', component: PageNotFoundComponent}
+];
